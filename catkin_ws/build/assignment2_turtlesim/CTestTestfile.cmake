85e7cc6754b1a710ceb388c4d6f6a7692c3ccc85
bsande6 <bsande6@g.clemson.edu>
2022-02-07 16:04:09 -0500

    catkin_ws

---
+++ catkin_ws/build/assignment2_turtlesim/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/bsande6/AuE8230Spring22_BradSanders/catkin_ws/src/assignment2_turtlesim
+# Build directory: /home/bsande6/AuE8230Spring22_BradSanders/catkin_ws/build/assignment2_turtlesim
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws/build/gtest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/bsande6/AuE8230Spring22_BradSanders/catkin_ws/build/gtest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
