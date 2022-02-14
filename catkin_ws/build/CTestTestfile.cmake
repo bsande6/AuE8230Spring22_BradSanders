5a49756767dfb7f1f200a61fd3a911cd763e3a0e
bsande6 <bsande6@g.clemson.edu>
2022-02-13 21:22:15 -0500

    Reseting github

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bsande6/AuE8230Spring22_BradSanders/catkin_ws/src
-# Build directory: /home/bsande6/AuE8230Spring22_BradSanders/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("assignment2_turtlesim")
--- catkin_ws/build/assignment2_turtlesim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bsande6/AuE8230Spring22_BradSanders/catkin_ws/src/assignment2_turtlesim
-# Build directory: /home/bsande6/AuE8230Spring22_BradSanders/catkin_ws/build/assignment2_turtlesim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
